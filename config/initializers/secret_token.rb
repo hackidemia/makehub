eeed1b73776bf12df79fb3bed803fc6abca0f5e2
Julius Volz <julius@soundcloud.com>
2013-05-24 21:33:52 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Makehub::Application.config.secret_key_base = '641f495364b348b18d3f5eb95df9ff97397c0eea2107ad45f2191641c74d20cb9f946d8df3849e536225e7c8e620e54d93722b74d41e90ed6afab6c2626721cd'
