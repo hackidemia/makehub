d34d9e9c335fb5aafdd839b513fc39ff76f2ce4e
Allard van Helbergen <allard@vanhelbergen.com>
2013-05-25 18:07:30 +0200

    Making active menu items (except for the nested one)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
     # check for ID because we always have a current user
     current_user.id.present?
   end
+
+  def nav_link(link_text, link_path, html_opts = {})
+	  active = current_page?(link_path) ? 'active' : ''
+
+	  content_tag(:li, :class => "#{html_opts[:class]} #{active}") do
+	    link_to link_text, link_path
+	  end
+	end
 end
