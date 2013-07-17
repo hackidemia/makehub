cc08771adc7d10632230fd881fbe9db120c18286
Stefania Druga <druga.stefania@gmail.com>
2013-07-17 12:58:14 +0100

    oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-provider :google_oauth2, ENV["227438588534.apps.googleusercontent.com"] || '', ENV["GTJlU4O_r5PNHLU9ZSuGrMzh"] || '' { access_type: "offline", approval_prompt: "" }
+provider :google_oauth2, ENV["227438588534.apps.googleusercontent.com"] || '', ENV["GTJlU4O_r5PNHLU9ZSuGrMzh"] || ''
 end
