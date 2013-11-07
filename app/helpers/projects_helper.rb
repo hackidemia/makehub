7fbdd8f174aac5cb221d5fe385d9734e99db28b2
Patricia Garcia <pat@patriciagarcia.me>
2013-11-07 12:13:29 +0100

    Extract select element generation code to helper method

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,8 @@
 module ProjectsHelper
+  def select_for_project(f, attr, html_params = {})
+    select_class = html_params.delete(:class)
+    html_params[:class] = select_class ? [select_class, 'span2'].join(' ') : 'span2'
+    f.select(attr, APPCONFIG['project'][attr.to_s].map { |i| [i, i] },
+      { prompt: attr.to_s.capitalize }, html_params)
+  end
 end
